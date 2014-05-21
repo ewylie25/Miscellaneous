8fdf835d1086edec72f53367ba53f9c25f066d76
Liz Wylie <ewylie25@gmail.com>
2014-05-21 12:44:43 -0500

    PHP or PHPStorm is weird with file names... figured out stats extension

---
+++ sbbrg_project/correlationcalculation.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * User: liz
+ * Date: 5/20/14
+ *
+ * TODO: PHP Statistics seems poorly documented/developed: proper way to integrate R/python with exec()?
+ */
+
+namespace sbbrg_project;
+
+
+class CorrelationCalculation {
+    public $price_type;
+    private $correlation;
+    private $google_data;
+    private $yahoo_data;
+
+    public function __construct($price_type){
+        # Set column of data to look at
+        $this->price_type = $price_type;
+    }
+    public function getCorrelation(){
+        $this->setCorrelation();
+        return $this->correlation;
+    }
+    private function setCorrelation(){
+        # retrieve data from My SQL
+        $this->setData();
+
+        # I was a little concerned about the stats extension because it isn't very documented... totally works fine
+        //$google_sd = $this->getStdDev($this->google_data);
+        //$yahoo_sd = $this->getStdDev($this->yahoo_data);
+        //$covariance = $this->getCovariance($this->google_data, $this->yahoo_data);
+        //$this->correlation = $covariance/($google_sd*$yahoo_sd);
+
+        # Calculate correlation using stats extension
+        $this->correlation = stats_stat_correlation($this->google_data, $this->yahoo_data);
+    }
+    private function setData(){
+        $this->google_data = $this->fromDB('google');
+        $this->yahoo_data = $this->fromDB('yahoo');
+    }
+    private function fromDB($table){
+        #TODO:Unify db parameters in settings file
+        $user = 'auser';
+        $pass = 'apass';
+        $temp = array();
+        try {
+            $dbh = new \PDO('mysql:host=localhost;dbname=test', $user, $pass);
+            foreach($dbh->query("SELECT $this->price_type from $table") as $value) {
+                array_push($temp, $value[0]);
+            }
+            $dbh = null;
+        } catch (\PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+        }
+        return $temp;
+    }
+    # I was a little concerned about the stats extension because it isn't very documented... totally works fine/ at least comparable to R
+   //private function getStdDev($data){
+   //     return stats_standard_deviation($data);
+   // }
+   // private function getCovariance($data1, $data2){
+   //     return stats_covariance($data1, $data2);
+   // }
+} 
--- sbbrg_project/main.php
+++ sbbrg_project/main.php
@@ -7,8 +7,29 @@
  */
 
 include 'helper_functions.php';
+include 'correlationcalculation.php';
 
 $success = \sbbrg_project\generate_database_from_CSV();
 if (!$success){
     die("Could not generate databases");
 }
+
+print "Open"."\n";
+$open = new \sbbrg_project\CorrelationCalculation('open');
+$open_corr = $open ->getCorrelation();
+print $open_corr."\n";
+
+print "Close"."\n";
+$close = new \sbbrg_project\CorrelationCalculation('close');
+$close_corr = $close ->getCorrelation();
+print $close_corr."\n";
+
+print "Low"."\n";
+$low = new \sbbrg_project\CorrelationCalculation('low');
+$low_corr = $low->getCorrelation();
+print $low_corr."\n";
+
+print "High"."\n";
+$high = new \sbbrg_project\CorrelationCalculation('high');
+$high_corr = $high->getCorrelation();
+print $high_corr."\n";
